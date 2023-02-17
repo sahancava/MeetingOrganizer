4b56a84dfbc79c8daf0ba5420c8fccd76a908b32
Şahan <57447079+sahancava@users.noreply.github.com>
2023-02-18 01:27:00 +0300

    no need for assert at testing process

--- hardhat/test/MeetingOrganizer.ts
+++ hardhat/test/MeetingOrganizer.ts
@@ -1,4 +1,4 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Contract } from 'ethers';
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
